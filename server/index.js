f443b42e7c46903c4b8c70deb45c7653d25fe4ca
heet18177 <heetmavani8177@gmail.com>
2026-01-09 21:04:55 +0530

    add..

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import connectDb from "./configs/db.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -10,25 +10,28 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set("trust proxy", 1);
 app.use(cookieParser());
 
-app.use(cors({origin : ["http://localhost:5173" , "https://blog-platform-client.onrender.com"] , credentials : true}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://blog-platform-client.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 
 import userRoute from "./routes/userRoute.js";
 import blogRoute from "./routes/blogRoute.js";
 import commentRoute from "./routes/commentRoute.js";
 
-app.use('/api/auth' , userRoute);
-app.use('/api/blogs' , blogRoute);
-app.use('/api/comments' , commentRoute);
+app.use("/api/auth", userRoute);
+app.use("/api/blogs", blogRoute);
+app.use("/api/comments", commentRoute);
 
-app.listen(port , ()=>{
-    console.log(`Server is running on port ${port}`);
-    connectDb();
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+  connectDb();
 });
-
-
-
-
-
-
